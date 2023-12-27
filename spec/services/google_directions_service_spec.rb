1ec5d5d5c599ca8a597c32c3c31fb22c6da62906
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-27 16:47:32 -0700

    Creates the test for making the API call to the google directions API by using a new "GoogleDirectionsService" class.

---
+++ spec/services/google_directions_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Google Directions API Service' do
+  it 'can get directions', :vcr do
+    driver = Driver.create(address: "12051 E Arizona Ave. Aurora, CO 80012")
+
+    ride = Ride.create(driver: driver, 
+            start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
+            end_address:"1550 S Potomac St, Aurora, CO 80012")
+          
+    goodle_directions = GoogleDirectionsService.new(ride.start_address, ride.end_address)
+
+    expect(goodle_directions).to eq("Success")
+    expect(goodle_directions).to include(ride.start_address)
+    expect(goodle_directions).to include(ride.end_address)
+  end
+end
