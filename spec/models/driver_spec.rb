f860ae85d54840fd5a66b39bde0bbba4a13a31d3
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-26 17:12:52 -0700

    Adds one to many relationship tests to the Driver & Ride spec files.

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Driver do
+  describe 'relationships' do
+    it { should have_many(:drives) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:address) }
   end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Ride do
+  describe 'relationships' do
+    it { should belong_to(:driver) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:start_address) }
     it { should validate_presence_of(:end_address) }
