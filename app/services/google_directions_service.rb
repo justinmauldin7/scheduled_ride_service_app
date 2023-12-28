287e8741eb23e063be29aac54a3fd21916f06193
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-28 11:44:37 -0700

    Refactors the GoogleDirectionsService & test to use a singleton class with the google api key set as a frozen constant on the class.

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -1,23 +1,20 @@
 class GoogleDirectionsService
-  def initialize(start_address, end_address)
-    @start_address = start_address
-    @end_address = end_address
-  end
+  API_KEY = ENV['GOOGLE_MAPS_API_KEY'].freeze
 
-  def get_directions_data
-    get_json("/maps/api/directions/json?origin=#{@start_address}&destination=#{@end_address}")
+  def self.get_directions_data(start_address, end_address)
+    get_json("/maps/api/directions/json?origin=#{start_address}&destination=#{end_address}")
   end
 
   private
 
-  def conn
+  def self.conn
     Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
-      faraday.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
+      faraday.params['key'] = API_KEY
       faraday.adapter  Faraday.default_adapter
     end
   end
 
-  def get_json(path)
+  def self.get_json(path)
     response = conn.get(path)
     JSON.parse(response.body, symbolize_names: true)
   end
--- spec/services/google_directions_service_spec.rb
+++ spec/services/google_directions_service_spec.rb
@@ -7,9 +7,8 @@ describe 'Google Directions API Service' do
     ride = Ride.create(driver: driver, 
             start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
             end_address:"1550 S Potomac St, Aurora, CO 80012")
-          
-    google_directions = GoogleDirectionsService.new(ride.start_address, ride.end_address)
-    api_response = google_directions.get_directions_data
+
+    api_response = GoogleDirectionsService.get_directions_data(ride.start_address, ride.end_address)
 
     api_response_start_address = api_response[:routes].first[:legs].first[:start_address]
     api_response_end_address = api_response[:routes].first[:legs].first[:end_address]
