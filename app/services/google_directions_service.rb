d167c3e94a225ec380191c3d6c1c820849b8c6eb
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-27 17:08:18 -0700

    Adds the helper methods to the GoogleDirectionsService needed to make the API call to the Google Directions API endpoint.

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -3,4 +3,22 @@ class GoogleDirectionsService
     @start_address = start_address
     @end_address = end_address
   end
+
+  def get_directions_data
+    get_json("/maps/api/directions/json?origin=#{@start_address}&destination=#{@end_address}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
+      faraday.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def get_json(path)
+    response = conn.get(path)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
