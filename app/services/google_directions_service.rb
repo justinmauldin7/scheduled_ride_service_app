dfa1c6ff9a35b02fe7cfc98c0de7d3b988d81ab9
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-28 16:27:54 -0700

    Updates the GoogleDirectionsService to take a third address being the drivers address as a waypoint to the API call & updates the test & VCR cassette with this change.

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -1,8 +1,8 @@
 class GoogleDirectionsService
   API_KEY = ENV['GOOGLE_MAPS_API_KEY'].freeze
 
-  def self.get_directions_data(start_address, end_address)
-    get_json("/maps/api/directions/json?origin=#{start_address}&destination=#{end_address}")
+  def self.get_directions_data(driver_address, start_address, end_address)
+    get_json("/maps/api/directions/json?origin=#{driver_address}&waypoints=#{start_address}&destination=#{end_address}")
   end
 
   private
--- spec/services/google_directions_service_spec.rb
+++ spec/services/google_directions_service_spec.rb
@@ -8,12 +8,16 @@ describe 'Google Directions API Service' do
             start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
             end_address:"1550 S Potomac St, Aurora, CO 80012")
 
-    api_response = GoogleDirectionsService.get_directions_data(ride.start_address, ride.end_address)
+    api_response = GoogleDirectionsService.get_directions_data(driver.address, ride.start_address, ride.end_address)
 
-    api_response_start_address = api_response[:routes].first[:legs].first[:start_address]
-    api_response_end_address = api_response[:routes].first[:legs].first[:end_address]
+    api_response_driver_address = api_response[:routes].first[:legs].first[:start_address]
+    api_response_start_address = api_response[:routes].first[:legs].last[:start_address]
+    api_response_end_address = api_response[:routes].first[:legs].last[:end_address]
 
     expect(api_response[:status]).to eq("OK")
+    # This expect is written different because the driver's address as it is saved to the driver record in the db,
+    # might not be the same format that is returned by the Google Directions API response.
+    expect(driver.address).to start_with(api_response_driver_address.split(",").first)
     expect(api_response_start_address).to include(ride.start_address)
     expect(api_response_end_address).to include(ride.end_address)
   end
