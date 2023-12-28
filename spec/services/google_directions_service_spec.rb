c7abe84fb7e0c24bc2057338bb31640b4dd95e03
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-27 17:22:35 -0700

    Updates the GoogleDirectionsService test with the right expects finding the right data nested in the Google Directions API response to make the test past.

--- spec/services/google_directions_service_spec.rb
+++ spec/services/google_directions_service_spec.rb
@@ -8,11 +8,14 @@ describe 'Google Directions API Service' do
             start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
             end_address:"1550 S Potomac St, Aurora, CO 80012")
           
-    goodle_directions = GoogleDirectionsService.new(ride.start_address, ride.end_address)
+    google_directions = GoogleDirectionsService.new(ride.start_address, ride.end_address)
     api_response = google_directions.get_directions_data
 
-    expect(api_response).to eq("Success")
-    expect(api_response).to include(ride.start_address)
-    expect(api_response).to include(ride.end_address)
+    api_response_start_address = api_response[:routes].first[:legs].first[:start_address]
+    api_response_end_address = api_response[:routes].first[:legs].first[:end_address]
+
+    expect(api_response[:status]).to eq("OK")
+    expect(api_response_start_address).to include(ride.start_address)
+    expect(api_response_end_address).to include(ride.end_address)
   end
 end
