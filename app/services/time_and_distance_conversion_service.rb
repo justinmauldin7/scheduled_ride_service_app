19f80114fbed005977346f6f76d2dd12577be54b
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-29 10:50:53 -0700

    Creates the TimeAndDistanceConversionService with test to convert the "duration" value we get back from the Google Directions API from seconds to hours.

---
+++ app/services/time_and_distance_conversion_service.rb
@@ -0,0 +1,25 @@
+class TimeAndDistanceConversionService
+  def initialize(integer_to_convert) 
+    @integer_to_convert = integer_to_convert
+  end
+
+  def convert_seconds_to_hours 
+    seconds_in_a_hour = 3600
+
+    # If we don't convert the integers into floats, any number that is retruned by the math 
+    # that is less than 1 will be returned as 0 instead of a decimal.
+    hours_as_float = @integer_to_convert.to_f / seconds_in_a_hour.to_f
+
+    format_decimal_places(hours_as_float)
+  end
+
+  private
+
+  def format_decimal_places(value)
+    # This will convert our float into a string & return only 2 decimal places. 
+    # EX: 0.0313888 turns into "0.03"
+    # It also makes sure it ends in a 0 if needed for consistant formatting.
+    # EX: 0.401 turns into "0.40"
+    '%.2f' % value
+  end
+end
---
+++ spec/services/time_and_distance_conversion_service_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Time And Distance Conversion Service' do
+  before :each do
+     @integer_to_convert = 113
+  end
+
+  it 'can convert seconds into hours', :vcr do
+    conversion_service = TimeAndDistanceConversionService.new(@integer_to_convert)
+    converted_value = conversion_service.convert_seconds_to_hours
+
+    expect(converted_value).to be_an(String)
+    expect(converted_value).to eq("0.03")
+
+    expect(converted_value).not_to be_an(Float)
+    expect(converted_value).not_to eq(0.03)
+  end
+end
