49dfa8ab77437c69417788b8612ed42fbb5a4bad
Justin Mauldin <justin.mauldin7@gmail.com>
2023-12-29 20:05:39 -0700

    Adds the "score" field on the Ride table for saving a ride's score as it is calculated from the data returned from the Google Directions API and can minimize API calls.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_001334) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_30_030217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_001334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "driver_id"
+    t.float "score"
     t.index ["driver_id"], name: "index_rides_on_driver_id"
   end
 
