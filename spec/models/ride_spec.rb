4672977c8f843bf606189aef7b136d0290533b0b
Justin Mauldin <justin.mauldin7@gmail.com>
2024-01-01 11:33:22 -0700

    Fixes a typo in the ride_spec.rb file.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -30,7 +30,7 @@ describe Ride do
     end
 
     it 'can prevent trying to set the ride score on a record if it already has one', :vcr do
-      expect { r@ride.set_ride_score }.to raise_error(StandardError, "ERROR: Ride score is already set.")
+      expect { @ride.set_ride_score }.to raise_error(StandardError, "ERROR: Ride score is already set.")
     end
   end  
 end
