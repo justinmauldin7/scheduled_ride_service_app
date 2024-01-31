7ce1920a8d2dece2a279de5bc11d40dee1ca33bb
Justin Mauldin <justin.mauldin7@gmail.com>
2024-01-31 13:39:22 -0700

    Updates the "ride_spec.rb" file to set "driver" & "ride" using "let" blocks instead of using instance variables.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -11,26 +11,26 @@ describe Ride do
   end
 
   describe 'instance methods' do
-    before :each do
-      @driver = Driver.create(address: "12051 E Arizona Ave. Aurora, CO 80012")
+    let(:driver) { Driver.create(address: "12051 E Arizona Ave. Aurora, CO 80012") }
 
-      @ride = Ride.create(driver: @driver, 
-              start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
-              end_address:"1550 S Potomac St, Aurora, CO 80012")
+    let(:ride) do
+        Ride.create(driver: driver, 
+                    start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
+                    end_address:"1550 S Potomac St, Aurora, CO 80012")
     end
 
     it 'can set the score field on a ride record', :vcr do
-      cleaned_route_data = GoogleDirectionsService.get_cleaned_ride_routing_data(@driver.address, @ride.start_address, @ride.end_address)
+      cleaned_route_data = GoogleDirectionsService.get_cleaned_ride_routing_data(driver.address, ride.start_address, ride.end_address)
 
       ride_score_service = RideScoreCalculatorService.new(cleaned_route_data)
       expected_ride_score = ride_score_service.get_ride_score.to_f
 
-      expect(@ride.score).to eq(expected_ride_score)
-      expect(@ride.score).not_to eq(nil)
+      expect(ride.score).to eq(expected_ride_score)
+      expect(ride.score).not_to eq(nil)
     end
 
     it 'can prevent trying to set the ride score on a record if it already has one', :vcr do
-      expect { @ride.set_ride_score }.to raise_error(StandardError, "ERROR: Ride score is already set.")
+      expect { ride.set_ride_score }.to raise_error(StandardError, "ERROR: Ride score is already set.")
     end
   end  
 end
