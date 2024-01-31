d10403228b26712be0ea9ee2f27656e453b0ae5d
Justin Mauldin <justin.mauldin7@gmail.com>
2024-01-31 14:10:21 -0700

    Updates the “google_directions_service_spec.rb” file to set “driver” & “ride” using “let” blocks instead of using instance variables.

--- spec/services/google_directions_service_spec.rb
+++ spec/services/google_directions_service_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe 'Google Directions API Service' do
-  before :each, :vcr do
-    @driver = Driver.create(address: "12051 E Arizona Ave. Aurora, CO 80012")
+  let(:driver) { Driver.create(address: "12051 E Arizona Ave. Aurora, CO 80012") }
 
-    @ride = Ride.create(driver: @driver, 
-            start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
-            end_address:"1550 S Potomac St, Aurora, CO 80012")
+  let(:ride) do 
+    Ride.create(driver: driver, 
+                start_address:"12200 E Mississippi Ave, Aurora, CO 80012",
+                end_address:"1550 S Potomac St, Aurora, CO 80012")
   end
   
   it 'can get raw directions data', :vcr do
-    api_response = GoogleDirectionsService.get_raw_directions_data(@driver.address, @ride.start_address, @ride.end_address)
+    api_response = GoogleDirectionsService.get_raw_directions_data(driver.address, ride.start_address, ride.end_address)
 
     api_response_driver_address = api_response[:routes].first[:legs].first[:start_address]
     api_response_start_address = api_response[:routes].first[:legs].last[:start_address]
@@ -19,13 +19,13 @@ describe 'Google Directions API Service' do
     expect(api_response[:status]).to eq("OK")
     # This expect is written different because the driver's address as it is saved to the driver record in the db,
     # might not be the same format that is returned by the Google Directions API response.
-    expect(@driver.address).to start_with(api_response_driver_address.split(",").first)
-    expect(api_response_start_address).to include(@ride.start_address)
-    expect(api_response_end_address).to include(@ride.end_address)
+    expect(driver.address).to start_with(api_response_driver_address.split(",").first)
+    expect(api_response_start_address).to include(ride.start_address)
+    expect(api_response_end_address).to include(ride.end_address)
   end
 
   it 'can get cleaned directions data', :vcr do
-    ride_data = GoogleDirectionsService.get_serialized_directions_data(@driver.address, @ride.start_address, @ride.end_address)
+    ride_data = GoogleDirectionsService.get_serialized_directions_data(driver.address, ride.start_address, ride.end_address)
 
     ride_data.each do |ride|
       expect(ride).to have_key(:distance)
@@ -49,7 +49,7 @@ describe 'Google Directions API Service' do
   end
 
   it 'can get cleaned distance & direction data (for ride routing)', :vcr do
-    ride_data = GoogleDirectionsService.get_cleaned_ride_routing_data(@driver.address, @ride.start_address, @ride.end_address)
+    ride_data = GoogleDirectionsService.get_cleaned_ride_routing_data(driver.address, ride.start_address, ride.end_address)
 
     ride_data.each do |ride|
       expect(ride.keys).to eq([:distance, :duration])
