f2f9c2ec164ae49e05c663c20f977677cec7668f
Justin Mauldin <justin.mauldin7@gmail.com>
2024-01-31 15:55:05 -0700

    Fixes the GoogleDirectionsService class to actually have private & public methods by changing how we are handling "self" on the class.

--- app/services/google_directions_service.rb
+++ app/services/google_directions_service.rb
@@ -1,65 +1,67 @@
 class GoogleDirectionsService
-  API_KEY = ENV['GOOGLE_MAPS_API_KEY'].freeze
-  GOOGLE_API_URL = 'https://maps.googleapis.com'.freeze
-
-  def self.get_raw_directions_data(driver_address, start_address, end_address)
-    # Here is a link to the Google Directions API Docs that shows what the response of this API call looks like: 
-    # https://developers.google.com/maps/documentation/directions/get-directions#DirectionsResponses
-    get_json("/maps/api/directions/json?origin=#{driver_address}&waypoints=#{start_address}&destination=#{end_address}")
-  end
-
-  def self.get_serialized_directions_data(driver_address, start_address, end_address)
-    # This method returns only a raw version of the "duration" & "distance" data found in the "legs" secion from the raw data.
-    # The data will look like this:
-    # [{:distance=>{:text=>"0.3 mi", :value=>531}, :duration=>{:text=>"2 mins", :value=>113}}, 
-    # {:distance=>{:text=>"1.3 mi", :value=>2058}, :duration=>{:text=>"5 mins", :value=>319}}]
-    raw_data = get_raw_directions_data(driver_address, start_address, end_address)
-
-    get_distance_and_duration_data(raw_data)
-  end
-
-  def self.get_cleaned_ride_routing_data(driver_address, start_address, end_address)
-    # This method returns only a cleaned up version of the "duration" & "distance" data found in the "legs" secion from the raw data.
-    # The data will look like this:
-    # [{:distance=>531, :duration=>113}, {:distance=>2058, :duration=>319}]
-    raw_distance_and_duration_data = get_serialized_directions_data(driver_address, start_address, end_address)
-
-    get_cleaned_distance_and_duration_data(raw_distance_and_duration_data)
-  end
-
-  private
-
-  def self.conn
-    Faraday.new(:url => GOOGLE_API_URL) do |faraday|
-      faraday.params['key'] = API_KEY
-      faraday.adapter  Faraday.default_adapter
+  class << self
+    API_KEY = ENV['GOOGLE_MAPS_API_KEY'].freeze
+    GOOGLE_API_URL = 'https://maps.googleapis.com'.freeze
+  
+    def get_raw_directions_data(driver_address, start_address, end_address)
+      # Here is a link to the Google Directions API Docs that shows what the response of this API call looks like: 
+      # https://developers.google.com/maps/documentation/directions/get-directions#DirectionsResponses
+      get_json("/maps/api/directions/json?origin=#{driver_address}&waypoints=#{start_address}&destination=#{end_address}")
     end
-  end
-
-  def self.get_json(path)
-    response = conn.get(path)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def self.get_distance_and_duration_data(raw_data)
-    route_legs = raw_data[:routes].first[:legs]
-    cleaned_route_array = []
-
-    route_legs.each do |leg|
-      distance_and_duration_hash = leg.slice(:distance, :duration)
-
-      cleaned_route_array << distance_and_duration_hash
+  
+    def get_serialized_directions_data(driver_address, start_address, end_address)
+      # This method returns only a raw version of the "duration" & "distance" data found in the "legs" secion from the raw data.
+      # The data will look like this:
+      # [{:distance=>{:text=>"0.3 mi", :value=>531}, :duration=>{:text=>"2 mins", :value=>113}}, 
+      # {:distance=>{:text=>"1.3 mi", :value=>2058}, :duration=>{:text=>"5 mins", :value=>319}}]
+      raw_data = get_raw_directions_data(driver_address, start_address, end_address)
+  
+      get_distance_and_duration_data(raw_data)
     end
-
-    cleaned_route_array
-  end 
-
-  def self.get_cleaned_distance_and_duration_data(raw_data)
-    raw_data.map do |route_data|
-      {
-        distance: route_data[:distance][:value],
-        duration: route_data[:duration][:value]
-      }
+  
+    def get_cleaned_ride_routing_data(driver_address, start_address, end_address)
+      # This method returns only a cleaned up version of the "duration" & "distance" data found in the "legs" secion from the raw data.
+      # The data will look like this:
+      # [{:distance=>531, :duration=>113}, {:distance=>2058, :duration=>319}]
+      raw_distance_and_duration_data = get_serialized_directions_data(driver_address, start_address, end_address)
+  
+      get_cleaned_distance_and_duration_data(raw_distance_and_duration_data)
+    end
+  
+    private
+  
+    def conn
+      Faraday.new(:url => GOOGLE_API_URL) do |faraday|
+        faraday.params['key'] = API_KEY
+        faraday.adapter  Faraday.default_adapter
+      end
+    end
+  
+    def get_json(path)
+      response = conn.get(path)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  
+    def get_distance_and_duration_data(raw_data)
+      route_legs = raw_data[:routes].first[:legs]
+      cleaned_route_array = []
+  
+      route_legs.each do |leg|
+        distance_and_duration_hash = leg.slice(:distance, :duration)
+  
+        cleaned_route_array << distance_and_duration_hash
+      end
+  
+      cleaned_route_array
+    end 
+  
+    def get_cleaned_distance_and_duration_data(raw_data)
+      raw_data.map do |route_data|
+        {
+          distance: route_data[:distance][:value],
+          duration: route_data[:duration][:value]
+        }
+      end
     end
   end
 end
